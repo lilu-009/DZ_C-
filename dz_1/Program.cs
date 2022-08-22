db5cb7f480f6323acd7c65472359c33679534b69
«olga» <lilu_009@mail.ru>
2022-08-22 12:10:55 +0800

    dz_1

---
+++ dz_1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 2
+
+// Console.Write("Enter the first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if(number1 > number2)
+// {
+//    Console.Write("Max: " +number1);
+// }
+// else
+// {
+//    Console.Write("Max: " +number2);
+// }
+
+
+
+// Задача 4
+
+ //Console.Write("Enter the first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the third number: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int max = number1;
+// if(number2 > max)
+// {
+//    max = number2;
+//    if(number3 > max) max = number3;
+// }
+// else if(number3 > max) max = number3;
+// Console.Write("Max: " +max);
+
+
+
+// Задача 6
+
+// Console.Write("Enter number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number % 2 == 0)
+// {
+//    Console.Write("Number is even");
+// }
+// else
+// {
+//    Console.Write("Odd number");
+//}
+
+
+
+// Задача 8
+
+Console.Write("Enter number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+while(number2 <= number1)
+{
+    Console.Write(number2 +",");
+    number2 = number2 + 2;
+}
