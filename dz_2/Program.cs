3590f64e268ad07897310b8d32e7db0dbcffc5a0
«olga» <lilu_009@mail.ru>
2022-08-23 22:06:01 +0800

    dz_2

---
+++ dz_2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 10
+
+// Console.Write("Enter number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number / 100 >= 1)
+// {
+//     int number1 = number / 10;
+//     int number2 = number1 % 10;
+
+//     Console.Write("Second digit: " +number2);
+// }
+// else
+// {
+//     Console.Write("The number is not three digits");
+// }
+
+
+
+// Задача 13
+
+// Console.Write("Enter number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number / 100 >= 1)
+// {
+//     int number1 = number % 10;
+
+//     Console.Write("Third digit: " +number1);
+// }
+// else
+// {
+//     Console.Write("No third digit");
+// }
+
+
+
+// Задача 15
+
+Console.Write("Enter the number of the day of the week: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 7 && number > 0)
+{
+    if(number > 5)
+    {
+        Console.Write("Yes");
+    }
+
+    else
+    {
+        Console.Write("No");
+    }
+}
+
+else
+{
+    Console.Write("No such day of the week");
+}
